9a107d86709e3584a87aca319f6d5f1cbd1a5260
Scott Schupbach <ssschupbach@gmail.com>
2019-04-15 16:39:35 -0500

    Add controller methods; start fleshing out index/style

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,35 @@
 class JobsController < ApplicationController
+  before_action :ensure_job, only: [:new]
+  before_action :current_job, only: [:show, :destroy]
+
   def index
+    @jobs = Job.all
+  end
+
+  def new
+  end
+
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+  def current_job
+    @job = Job.find(params[:id])
+  end
+
+  def ensure_job
+    @job = Job.new unless @job
   end
 end
